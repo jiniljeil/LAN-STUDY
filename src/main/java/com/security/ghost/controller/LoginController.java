20170c46c301e4b3f5e85197fb3f99192d86d32c
김진일 <jiniljeil1@naver.com>
2021-08-26 04:15:08 +0900

    SecureCoding

---
+++ src/main/java/com/security/ghost/controller/LoginController.java
@@ -0,0 +1,63 @@
+package com.security.ghost.controller;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.security.ghost.Crypto;
+import com.security.ghost.dao.UserDAO;
+import com.security.ghost.dto.UserDTO;
+
+@Controller 
+public class LoginController {
+	
+	@Autowired
+	UserDAO userDAO; 
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ModelAndView loginOk(HttpServletRequest request, Model model, HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		String userid = request.getParameter("userID"); 
+		String userpw = request.getParameter("userPW"); 
+		 
+		if (userid != null && userpw != null) {
+			byte[] salt = userDAO.getSalt(userDAO.getID(userid)); 
+			userpw = Crypto.encryptSHA256(userpw, salt);
+
+			UserDTO userDTO = userDAO.getUser(new UserDTO(userid, userpw));
+			if(userDTO != null) {
+				session.setAttribute("LOGIN_USER", userDTO); 
+				model.addAttribute("u", userDTO);
+				String pToken = request.getParameter("param_csrf_token"); 
+				String sToken = (String)session.getAttribute("SESSION_CSRF_TOKEN"); 
+				if (pToken != null && pToken.equals(sToken)) {
+					mav.setViewName("redirect:/menu");
+					return mav;
+				}
+			} 
+			// 로그인 실패 alert 띄우기 
+			mav.setViewName("redirect:/error/loginError");
+		}
+		else mav.setViewName("redirect:/error/loginError");;
+		return mav;
+	}
+	
+//	@RequestMapping(value="/board")
+//    public String getBoard(Model mdel) throws ServletException, IOException {		
+//		List<UserDTO> userList = userDAO.getUserList();
+//		model.addAttribute("list", userList );
+//		return "homePage";
+//    }
+}
