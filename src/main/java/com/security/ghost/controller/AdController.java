e57b4feee9d05934cab30f3eda3ab3d9f9fa8613
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-26 04:31:49 +0900

    강제push

--- src/main/java/com/security/ghost/controller/AdController.java
+++ src/main/java/com/security/ghost/controller/AdController.java
@@ -3,6 +3,7 @@ package com.security.ghost.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import com.security.ghost.SecurityUtil;
 import com.security.ghost.dao.AdDAO;
 import com.security.ghost.dto.AdDTO;
 import com.security.ghost.dto.BoardDTO;
+import com.security.ghost.dto.UserDTO;
 
 @Controller
 public class AdController {
@@ -22,32 +24,32 @@ public class AdController {
 	@Autowired
 	AdDAO adDAO ;
 	
-	@RequestMapping(value="/make_study", method=RequestMethod.POST) 
-	public ModelAndView BoardOk(HttpServletRequest request) {
+	@RequestMapping(value="/upload", method=RequestMethod.POST) 
+	public ModelAndView AdOk(HttpServletRequest request, HttpSession session) {
 		ModelAndView mav = new ModelAndView(); 
-		
 		String title = request.getParameter("title"); 
-		
-		// TODO Session 유저 권한 체크 및 유저 ID 포함하여 디비에 저장 
-		
-		// TODO 이미지 처리 업로드 ?
-		
 		String content = request.getParameter("content"); 
 		
 		if (title != null && content != null) {
-			
-			// XSS BLOCK 
-			// encodeforhtml 
-			title = SecurityUtil.HTML_Filter(title); 
-			content = SecurityUtil.HTML_Filter(content); 
+			// TODO Session 유저 권한 체크 및 유저 ID 포함하여 디비에 저장 
+			UserDTO userDTO = (UserDTO)session.getAttribute("LOGIN_USER"); 
 
-			AdDTO adDTO = new AdDTO(); 
-			// TODO user_id group_id setting 
-			adDTO.setTitle(title); 
-			adDTO.setContent(content);
-			
-			adDAO.uploadAd(adDTO);
-			mav.setViewName("redirect:/homePage");
+			if (userDTO != null) {
+				// XSS BLOCK 
+				// encodeforhtml 
+				title = SecurityUtil.HTML_Filter(title); 
+				content = SecurityUtil.HTML_Filter(content); 
+
+				AdDTO adDTO = new AdDTO(); 
+				// TODO user_id group_id setting 
+				adDTO.setUser_id(userDTO.getId());
+				adDTO.setGroup_id(1);
+				adDTO.setTitle(title); 
+				adDTO.setContent(content);
+				
+				adDAO.uploadAd(adDTO);
+				mav.setViewName("redirect:/homePage");
+			}
 		}else {
 			// Q. 업로드 실패 혹은 제목 및 내용 입력 요구 
 			// 어디로 보낼 것 인가? 
@@ -56,10 +58,10 @@ public class AdController {
 		return mav;
 	}
 	
-	@RequestMapping(value="/AdStudy", method=RequestMethod.GET) 
-	public String BoardStudy(HttpServletRequest request, Model model) {
+	@RequestMapping(value="/homePage", method=RequestMethod.GET) 
+	public String AdStudy(HttpServletRequest request, Model model) {
 	     
-		// TODO: session으로 사용자의 정보를 가지고 가지고 있지 않는 group 들을 뿌려준다.
+		// session으로 사용자의 정보를 가지고 가지고 있지 않는 group 들을 뿌려준다.
 		List<BoardDTO> adList = adDAO.getAdList();
 		
 		model.addAttribute("AdList", adList);
