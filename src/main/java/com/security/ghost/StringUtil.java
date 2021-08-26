a9eb5b93e40fff94fc347281c6bcf9aa14cae472
qpalzmm22 <qpalzmm22@gmail.com>
2021-08-26 10:44:15 +0900

    fix random to secure random

--- src/main/java/com/security/ghost/StringUtil.java
+++ src/main/java/com/security/ghost/StringUtil.java
@@ -1,17 +1,21 @@
 package com.security.ghost;
 
+import java.security.SecureRandom;
 
 public class StringUtil {
 	public String randomAlphanumericStringGenerator(int len) {
         String AlphaNumericString = "0123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         
         StringBuilder sb = new StringBuilder(len);
-  
-        for (int i = 0; i < len; i++) {  
-            int index = (int)(AlphaNumericString.length()* Math.random());
-            sb.append(AlphaNumericString.charAt(index));
-        }
-  
+		SecureRandom secureRandom = new SecureRandom();
+	    
+	    for (int i = 0; i < len; i++) {  
+	        int index = secureRandom.nextInt(AlphaNumericString.length());
+	        sb.append(AlphaNumericString.charAt(index));
+	    }
+	    System.out.println(sb.toString());
+			
+
         return sb.toString();
 		}
 }
