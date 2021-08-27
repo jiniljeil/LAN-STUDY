40473ac9418f13c1a77a01783555ee216a8439e5
heejuShin <49302519+heejuShin@users.noreply.github.com>
2021-08-27 10:06:56 +0900

    Update SecureCodingApplication.java

--- src/main/java/com/security/ghost/SecureCodingApplication.java
+++ src/main/java/com/security/ghost/SecureCodingApplication.java
@@ -2,16 +2,10 @@ package com.security.ghost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SecureCodingApplication {
-	
-	//@Override
-	//protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	//	return builder.sources(SecureCodingApplication.class);
-	//}
-	
+
    public static void main(String[] args) {
       SpringApplication.run(SecureCodingApplication.class, args);
    }
